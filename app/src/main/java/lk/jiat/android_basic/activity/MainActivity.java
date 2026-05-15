92eab4c337ac226298aa1fa3f0e2db16a34ea2a9
kalharaweragoda <kalharaweragoda@gmail.com>
2026-05-16 05:15:16 +0530

    Override

--- app/src/main/java/lk/jiat/android_basic/activity/MainActivity.java
+++ app/src/main/java/lk/jiat/android_basic/activity/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
