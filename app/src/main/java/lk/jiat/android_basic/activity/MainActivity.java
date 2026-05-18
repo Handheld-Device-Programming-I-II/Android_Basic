28c86320f13b10e4acb5f6710e653a6e5b718057
kalharaweragoda <kalharaweragoda@gmail.com>
2026-05-18 15:41:01 +0530

    OnClickView

--- app/src/main/java/lk/jiat/android_basic/activity/MainActivity.java
+++ app/src/main/java/lk/jiat/android_basic/activity/MainActivity.java
@@ -2,6 +2,7 @@ package lk.jiat.android_basic.activity;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.annotation.Nullable;
@@ -16,5 +17,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button submitBtn = findViewById(R.id.submitBtn);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
