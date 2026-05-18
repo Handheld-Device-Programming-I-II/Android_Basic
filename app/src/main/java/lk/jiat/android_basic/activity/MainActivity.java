e6e1e5f2f7bcad52b2538bf0da136a8b705a286f
kalharaweragoda <kalharaweragoda@gmail.com>
2026-05-18 16:36:24 +0530

    testing_message

--- app/src/main/java/lk/jiat/android_basic/activity/MainActivity.java
+++ app/src/main/java/lk/jiat/android_basic/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,7 +21,8 @@ public class MainActivity extends AppCompatActivity {
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Toast.makeText(MainActivity.this,
+                        R.string.testing_messa)
             }
         });
     }
